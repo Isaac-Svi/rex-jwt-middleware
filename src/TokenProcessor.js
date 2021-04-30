0a16a3be0fa97a577beda56868c6dd62c7ef56ea
Isaac Svi <yitzchaksviridyuk@gmail.com>
2021-04-30 12:11:27 +0300

    fix expires option in sendRefreshToken to be in date format

--- src/TokenProcessor.js
+++ src/TokenProcessor.js
@@ -30,7 +30,7 @@ class TokenProcessor {
       cookie.serialize(cookieName, token, {
         path: route,
         httpOnly: true,
-        expires: !token ? 1 : new Date(Date.now() + exp * 1000),
+        expires: new Date(Date.now() + (!token ? 1 : exp * 1000)),
       })
     )
   }
